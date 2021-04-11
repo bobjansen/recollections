4d3383e55f68fb0b99bc19c55e7127853f3bec30
Bob Jansen <bobjansen@gmail.com>
2021-04-11 14:34:44 +0200

    Don't use a pointer to store innerHeap in PriorityQueue

--- src/priority_queue.h
+++ src/priority_queue.h
@@ -7,35 +7,30 @@ using namespace Rcpp;
 //' @export
 class PriorityQueue {
 private:
-  std::priority_queue<int> * innerHeap;
+  std::priority_queue<int> innerHeap;
 
 public:
-  PriorityQueue() : innerHeap(new std::priority_queue<int>()) {};
-  ~PriorityQueue() {
-    delete innerHeap;
-  }
-
   bool empty() const {
-    return innerHeap->empty();
+    return innerHeap.empty();
   }
 
   void clear() {
-    while (!innerHeap->empty()) {
-      innerHeap->pop();
+    while (!innerHeap.empty()) {
+      innerHeap.pop();
     }
   }
 
   int size() const {
-    return innerHeap->size();
+    return innerHeap.size();
   }
 
   void push(int value) {
-    innerHeap->push(value);
+    innerHeap.push(value);
   }
 
   void pop() {
     if (!empty()) {
-      innerHeap->pop();
+      innerHeap.pop();
     } else {
       throw std::length_error("Priority Queue empty, can't pop()");
     }
@@ -45,12 +40,12 @@ public:
     if (empty()) {
       throw std::length_error("Priority Queue empty, can't top()");
     }
-    return innerHeap->top();
+    return innerHeap.top();
   }
 
   int topAndPop() {
     int topValue = top();
-    innerHeap->pop();
+    innerHeap.pop();
     return topValue;
   }
 };
