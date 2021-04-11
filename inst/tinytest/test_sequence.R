5b75848e619a9f7436eba3b40b439dba9db28835
Bob Jansen <bobjansen@gmail.com>
2021-04-11 13:03:10 +0200

    Set integers correctly and fix whitespace

--- inst/tinytest/test_sequence.R
+++ inst/tinytest/test_sequence.R
@@ -2,43 +2,31 @@ library(recollections)
 
 # Integer sequence ----
 intSeq <- recollections::integerSequence()
-expect_equal(
-  capture.output(print(intSeq)),
-  "[1] \"Lazy Sequence\""
-)
+expect_equal(capture.output(print(intSeq)), "[1] \"Lazy Sequence\"")
 expect_equal(nextItem(intSeq), 1L)
-expect_equal(unlist(take(intSeq, 10)), 2:11L)
+expect_equal(unlist(take(intSeq, 10L)), 2:11)
 expect_equal(getIndex(intSeq), 11L)
 
 # Step sequence ----
 stepSeq <- recollections::stepSequence(10L, 2L)
-expect_equal(
-  capture.output(print(stepSeq)),
-  "[1] \"Lazy Sequence\""
-)
+expect_equal(capture.output(print(stepSeq)), "[1] \"Lazy Sequence\"")
 expect_equal(nextItem(stepSeq), 10L)
-expect_equal(unlist(take(stepSeq, 10)), 1:10L * 2L + 10L)
+expect_equal(unlist(take(stepSeq, 10L)), 1:10 * 2L + 10L)
 expect_equal(getIndex(stepSeq), 11L)
 
 # Cycle sequence ----
 cycleSeq <- recollections::cycleSequence(1L, 4L, 2L)
-expect_equal(
-  capture.output(print(cycleSeq)),
-  "[1] \"Lazy Sequence\""
-)
+expect_equal(capture.output(print(cycleSeq)), "[1] \"Lazy Sequence\"")
 expect_equal(nextItem(cycleSeq), 1L)
-expect_equal(unlist(take(cycleSeq, 3)), c(3L, 2L, 1L))
-expect_equal(getIndex(cycleSeq), 4)
+expect_equal(unlist(take(cycleSeq, 3L)), c(3L, 2L, 1L))
+expect_equal(getIndex(cycleSeq), 4L)
 
 # Prime sequence ----
 primeSeq <- recollections::primeSequence()
-expect_equal(
-  capture.output(print(primeSeq)),
-  "[1] \"Lazy Sequence\""
-)
+expect_equal(capture.output(print(primeSeq)), "[1] \"Lazy Sequence\"")
 expect_equal(nextItem(primeSeq), 2L)
-expect_equal(unlist(take(primeSeq, 4)), c(3L, 5L, 7L, 11L))
-expect_equal(getIndex(primeSeq), 5)
+expect_equal(unlist(take(primeSeq, 4L)), c(3L, 5L, 7L, 11L))
+expect_equal(getIndex(primeSeq), 5L)
 
 primeSeq <- recollections::primeSequence()
 expect_equal(
